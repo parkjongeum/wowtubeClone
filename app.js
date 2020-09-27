541765dd8819f8779efb04a56ff330b94e481496
박종음 <qkrwhddma@naver.com>
2020-09-27 18:27:58 +0900

    02.Searching

--- app.js
+++ app.js
@@ -7,15 +7,17 @@ import userRouter from "./routers/userRouter";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 const app = express();
 
-app.set("view engine", "pug");
 app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(localsMiddleware)
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
